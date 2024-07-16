fe2ef7acc2552a66b4a6918b16964460c1607228
gvweston2000 <gvweston2000@icloud.com>
2024-07-16 11:22:02 +0100

    Update views

--- app/models/validity.rb
+++ app/models/validity.rb
@@ -1,3 +1,15 @@
 class Validity < ApplicationRecord
     has_many :votes
+
+    def pre_measure?
+        measure == 'pre'
+    end
+
+    def during_measure?
+        measure == 'during'
+    end
+
+    def post_measure?
+        measure == 'post'
+    end
 end
